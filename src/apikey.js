a959957a1f5b80219ecf16fcaa462ffbcde486ec
Lutz Rosema <terabaud@gmail.com>
2017-02-20 17:14:59 +0100

    generated by `create-react-app`

---
+++ src/apikey.js
@@ -0,0 +1,4 @@
+// keys are specific to the URL https://terabaud.github.io/react-movie-app
+
+export const apiKey = "1fe8d9db1b44c3735757152690007485";
+export const authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZmU4ZDlkYjFiNDRjMzczNTc1NzE1MjY5MDAwNzQ4NSIsInN1YiI6IjU4YWIwOTNkYzNhMzY4NmVlOTAwNDBiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HzJqJx7n3Q9-NWdDA2ljhywNKgTvciBHUU-JnAPbGQc";
