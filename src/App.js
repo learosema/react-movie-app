d4e2f03fc3a0adf9234ebfe43b6e84e183815600
Lutz Rosema <terabaud@gmail.com>
2017-03-15 18:27:47 +0100

    Move header markup to seperate stateless component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { apiKey } from './apikey';
 import './App.css';
 import axios from 'axios';
-import { BrowserRouter as Router, NavLink as Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Header } from './Header';
 
 const Home = () => <h1>Home</h1>
 
@@ -27,21 +28,7 @@ class App extends Component {
     return (
       <Router>
       <div className="site">
-        <header className="site-header">
-          <div className="logo"><Link to="/">logo</Link></div>
-          <nav className="site-nav">
-            <ul>
-              <li><Link activeClassName="active" to={"/discover/top-rated"}>Top rated</Link></li>
-              <li><Link activeClassName="active" to={"/discover/most-popular"}>Most popular</Link></li>
-              <li><Link activeClassName="active" to={"/discover/upcoming"}>Upcoming movies</Link></li>
-              <li><Link activeClassName="active" to={"/discover/now-playing"}>Now playing</Link></li>
-              <li><Link activeClassName="active" to={"/search"}>Search</Link></li>
-            </ul>
-          </nav>
-          <div className="powered-by">
-            powered by <a href="https://www.themoviedb.org/">themoviedb</a>
-          </div>
-        </header>
+        <Header />
         <div class="site-main">
           <Route exact path="/" component={Home} />
           <Route path="/discover/:category" render={ ({match}) =>  (
@@ -54,7 +41,5 @@ class App extends Component {
     );
   }
 }
-
-
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import { React } from 'react'
+import { NavLink as Link } from 'react-router-dom'
+
+const Header = () => (
+	<header className="site-header">
+          <div className="logo"><Link to="/">logo</Link></div>
+          <nav className="site-nav">
+            <ul>
+              <li><Link activeClassName="active" to={"/discover/top-rated"}>Top rated</Link></li>
+              <li><Link activeClassName="active" to={"/discover/most-popular"}>Most popular</Link></li>
+              <li><Link activeClassName="active" to={"/discover/upcoming"}>Upcoming movies</Link></li>
+              <li><Link activeClassName="active" to={"/discover/now-playing"}>Now playing</Link></li>
+              <li><Link activeClassName="active" to={"/search"}>Search</Link></li>
+            </ul>
+          </nav>
+          <div className="powered-by">
+            powered by <a href="https://www.themoviedb.org/">themoviedb</a>
+          </div>
+    </header>)
+
+export default Header
